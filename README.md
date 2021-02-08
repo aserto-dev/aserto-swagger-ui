69048576942989a54318d7b3581be81a12cc189e
Omri Gazitt <ogazitt@gmail.com>
2021-02-08 14:09:45 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Aserto Swagger-UI 
+
+Aserto-themed Swagger-UI React component
+
+## Installation
+
+Yarn: `yarn install @aserto/aserto-swagger-ui` 
+
+NPM: `npm install @aserto/aserto-swagger-ui`
+
+## Usage
+
+```javascript
+import AsertoSwaggerUI from '@aserto/aserto-swagger-ui'
+
+const View = ({url}) => 
+  <AsertoSwaggerUI url={url} />
+
+export default View
+```
---
+++ src/AsertoSwaggerUI.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import SwaggerUI from 'swagger-ui'
+import 'swagger-ui/dist/swagger-ui.css'
+import './AsertoSwaggerUI.css'
+
+const AsertoSwaggerUI = ({url}) => {
+  useEffect(() => {
+    SwaggerUI({ 
+      dom_id: '#aserto-swagger-ui',
+      url,
+      defaultModelsExpandDepth: -1,
+      syntaxHighlight: {
+        activate: true,
+        theme: 'tomorrow-night'
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <div id='aserto-swagger-ui' />
+    </>
+  )
+}
+
+export default AsertoSwaggerUI
